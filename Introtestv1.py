3b66c44900070c8dfe03585f28a3ae1a2ec1023e
LazyTang <50754881+myhaohao520@users.noreply.github.com>
2022-12-24 15:05:56 -0800

    Update Introtestv1.py

--- Introtestv1.py
+++ Introtestv1.py
@@ -3,7 +3,8 @@
 #Title: Introvert or Extrovert Test v1
 
 
-#Below is for Linux users to allow program to be executable: ./intr>#Copy and paste it before the title!
+#Below is for Linux users to allow program to be executable: ./introtest.py
+#Copy and paste it before the title!
 
 #!/usr/bin/env python3
 
@@ -17,33 +18,38 @@ print("For the questions below, enter y for YES and n for NO")
 #Questions to determine if the user is Introvert or Extrovert
 
 while True:
-  question_1 = input("Question 1: Are you active in more than two d>  if question_1 == "y":
+  question_1 = input("Question 1: Are you active in more than two different group chat? ")
+  if question_1 == "y":
     answers["yes"] += 1
   elif question_1 == "n":
     answers["no"] += 1
                      
 while True:
-  question_2 = input("Question 2: When a coworker/friend asks you t>  if question_2 == "y":
+  question_2 = input("Question 2: When a coworker/friend asks you to go have a drink after work, do you want to go?")
+  if question_2 == "y":
     answers["yes"] += 1
   elif question_2 == "n":
     answers["no"] += 1
 
 while True:
-  question_3 =  input("Question 3: Are you a fan of last-minute pla>  if question_3 == "y":
+  question_3 =  input("Question 3: Are you a fan of last-minute plans?") 
+  if question_3 == "y":
     answer["yes"] += 1
   elif question_3 == "n":
     answers["no"] += 1
 
 while True:
-  question_4 =  input("Question 4: Are you a fan of being on camera>  if question_4 == "y":
+  question_4 =  input("Question 4: Are you a fan of being on camera?")
+  if question_4 == "y":
     answer["yes"] += 1
   elif question_4 == "n":
     answers["no"] += 1
 
 while True:
-  question_5 =  input("Question 5: Do you thrive when you're in cha>  if question_5 == "y":
+  question_5 =  input("Question 5: Do you thrive when you're in charge of a project?")
+  if question_5 == "y":
     answer["yes"] += 1
-                  elif question_5 == "n":
+  elif question_5 == "n":
     answers["no"] += 1
 
 while True:
@@ -54,13 +60,15 @@ while True:
     answers["no"] += 1
 
 while True:
-  question_7 =  input("Question 7: Do you act first without thinkin>  if question_7 == "y":
+  question_7 =  input("Question 7: Do you act first without thinking?")  
+  if question_7 == "y":
     answer["yes"] += 1
   elif question_7 == "n":
     answers["no"] += 1
 
 while True:
-  question_8 =  input("Question 8: Do you usually eat lunch in a la>  if question_8 == "y":
+  question_8 =  input("Question 8: Do you usually eat lunch in a large group?")
+  if question_8 == "y":
     answer["yes"] += 1
   elif question_8 == "n":
     answers["no"] += 1
