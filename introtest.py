3c9e741db22cf3251a41299545a88fb237f755dc
jianhao <lo.smith281@gmail.com>
2022-12-29 15:52:22 -0800

    1. Fixed typos for question_3 to question 8, from answer["yes"] to answers["yes"]. 2. Added break keywords at the end of all while loops to continue to each question. 4. Added " Answer: " at the end of each question to give space for users to type answer. 4. Fixed syntax error and symobols in introvert_or_extrovert_test(answers) function, 	from: answers[yes] >= answers[no]:	to: answers["yes"] > answers["no"]: 	from: answers[yes] <= answers[no]:	to: asnwers["yes"] < answers["no"]: 5. Added code at the end to call introvert_or_extrovert(answers). 6. Added new line \n to print("For the questions below, enter y for YES and n for NO") and print("Results ").

--- introtest.py
+++ introtest.py
@@ -12,7 +12,7 @@
 
 
 #Tell user to enter y for yes and n for no
-print("For the questions below, enter y for YES and n for NO")
+print("For the questions below, enter y for YES and n for NO\n")
 
 
 #Create a dictionary to store the answers to the questions
@@ -22,68 +22,78 @@ answers = {"yes": 0, "no": 0}
 #Questions to determine if the user is Introvert or Extrovert
 
 while True:
-  question_1 = input("Question 1: Are you active in more than two different group chat? ")
+  question_1 = input("Question 1: Are you active in more than two different group chat? Answer: ")
   if question_1 == "y":
     answers["yes"] += 1
   elif question_1 == "n":
     answers["no"] += 1
+  break
 
 while True:
-  question_2 = input("Question 2: When a coworker/friend asks you to go have a drink after work, do you want to go?")
+  question_2 = input("Question 2: When a coworker/friend asks you to go have a drink after work, do you want to go? Answer: ")
   if question_2 == "y":
     answers["yes"] += 1
   elif question_2 == "n":
     answers["no"] += 1
+  break
 
 while True:
-  question_3 =  input("Question 3: Are you a fan of last-minute plans?") 
+  question_3 =  input("Question 3: Are you a fan of last-minute plans? Answer: ") 
   if question_3 == "y":
-    answer["yes"] += 1
+    answers["yes"] += 1
   elif question_3 == "n":
     answers["no"] += 1
+  break
 
 while True:
-  question_4 =  input("Question 4: Are you a fan of being on camera?")
+  question_4 =  input("Question 4: Are you a fan of being on camera? Answer: ")
   if question_4 == "y":
-    answer["yes"] += 1
+    answers["yes"] += 1
   elif question_4 == "n":
     answers["no"] += 1
+  break
 
 while True:
-  question_5 =  input("Question 5: Do you thrive when you're in charge of a project?")
+  question_5 =  input("Question 5: Do you thrive when you're in charge of a project? Answer: ")
   if question_5 == "y":
-    answer["yes"] += 1
+    answers["yes"] += 1
   elif question_5 == "n":
     answers["no"] += 1
+  break
 
 while True:
-  question_6 =  input("Question 6: Do you send the first message?")
+  question_6 =  input("Question 6: Do you send the first message? Answer: ")
   if question_6 == "y":
-    answer["yes"] += 1
+    answers["yes"] += 1
   elif question_6 == "n":
     answers["no"] += 1
+  break
 
 while True:
-  question_7 =  input("Question 7: Do you act first without thinking?")  
+  question_7 =  input("Question 7: Do you act first without thinking? Answer: ")  
   if question_7 == "y":
-    answer["yes"] += 1
+    answers["yes"] += 1
   elif question_7 == "n":
     answers["no"] += 1
+  break
 
 while True:
-  question_8 =  input("Question 8: Do you usually eat lunch in a large group?")
+  question_8 =  input("Question 8: Do you usually eat lunch in a large group? Answer: ")
   if question_8 == "y":
-    answer["yes"] += 1
+    answers["yes"] += 1
   elif question_8 == "n":
     answers["no"] += 1
+  break
 
 
 #Function to determine whether the user is Introvert or Extrovert
 def introvert_or_extrovert_test(answers):
-  print("Results: ")
-  if answers[yes] >= answers[no]:
+  print("\nResults: ")
+  if answers["yes"] > answers["no"]:
     print("Unfortunately you are an Extrovert.")
-  elif answers[yes] <= answers[no]:
+  elif answers["yes"] < answers["no"]:
     print("Congratulations, you are an Introvert!")
   else:
     print("Oh interesting, you are an ambivert.")   
+
+introvert_or_extrovert_test(answers)
